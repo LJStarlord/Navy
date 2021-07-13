5ba57db8fb4001998bc64d0cead05cc722f56c44
aryan <aryanrashijain@gmail.com>
2021-07-13 01:00:03 -0400

    slider animation and spacebar listener

---
+++ index.js
@@ -0,0 +1,97 @@
+var board = [];
+var DisplayBoard = document.getElementById("board");
+var keyPressed;
+
+for (let i = 0; i < 15; i++) {
+    var holder = [];
+    for (let j = 0; j < 7; j++) {
+        holder[j] = "";
+    }
+    board[i] = holder;
+}
+
+console.log(board);
+
+
+for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+        var newSquare = document.createElement("div");
+        newSquare.setAttribute("class", "square");
+        DisplayBoard.appendChild(newSquare);
+        board[i][j] = newSquare;
+    }
+
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+function check() {
+    document.onkeydown = function(e) {
+        if (e.key === " ") {
+            keyPressed = e.key;
+        }
+
+    }
+}
+
+
+var end = false;
+async function checking() {
+    check();
+    var counter = 0;
+
+    while (!keyPressed) {
+
+        if (counter > 2) {
+            var square = board[board.length - 1][counter - 3];
+            square.setAttribute("class", "square");
+        }
+        if (counter === 0) {
+            var square = board[board.length - 1][3];
+            square.setAttribute("class", "square");
+            end = false;
+        }
+
+        if (counter === board[0].length) {
+
+            end = true;
+            counter = 4;
+        }
+
+
+
+
+        var square = board[board.length - 1][counter];
+        square.setAttribute("class", "checked square");
+
+
+
+        if (end === true) {
+
+
+
+            counter--;
+
+
+
+            if (counter < 3) {
+                var square = board[board.length - 1][counter + 4];
+                square.setAttribute("class", "square");
+            }
+
+        } else {
+
+            counter++;
+        }
+
+
+        await sleep(500);
+    }
+
+}
+
+
+checking();
